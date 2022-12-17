6fbeb2859dfbd544721dc75247dc2f03853d3d5a
Sibghat65 <rajasibghat2@gmail.com>
2022-12-17 01:58:18 -0800

    Material Design Spinner

---
+++ app/src/main/java/com/example/androidsolutions/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.androidsolutions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.example.androidsolutions.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        List<String> fruitsList=new ArrayList<>();
+        fruitsList.add("Mango");
+        fruitsList.add("Apple");
+        fruitsList.add("Banana");
+        fruitsList.add("Guava");
+        fruitsList.add("Grapes");
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,fruitsList);
+        binding.designSpinner.setAdapter(adapter);
+        binding.designSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You Clicked "+fruitsList.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+}
